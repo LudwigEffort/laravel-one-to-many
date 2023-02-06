e9126765797b5ff0f5632ad5d8c82460afd3cc96
LudwigEffort <dr.feynman65@gmail.com>
2023-02-06 13:11:59 +0100

    add lorem picsum seeder (didn't work), fix bug with uploade img

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -5,6 +5,7 @@ use App\Post;
 //use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class PostSeeder extends Seeder
 {
@@ -19,13 +20,17 @@ class PostSeeder extends Seeder
 
         for ($i=0; $i < 100; $i++) {
             $title = $faker->words(rand(3, 7), true);
+            //$loremImages = Storage::files('lorempicsum');
+            //$img_path = Storage::put('uploads', $faker->randomElement($loremImages)); //TODO: risolvere il problema dei permessi delle img
+
             Post::create([
                 'category_id' => $faker->randomElement($categories)->id, // cosi preandiamo casualmente gli id degli elementi della variablile (in questo caso le categorie)
-                'slug'    => Post::getSlug($title),
-                'title'   => $title,
-                'image'   => 'https://picsum.photos/id/'. rand(0, 1000) . '/500/400',
-                'content' => $faker->paragraphs(rand(1, 10), true),
-                'excerpt' => $faker->paragraphs(1, true),
+                'slug'          => Post::getSlug($title),
+                'title'         => $title,
+                'image'         => 'https://picsum.photos/id/'. rand(0, 1000) . '/500/400',
+                //'uploaded_img'  => $img_path,
+                'content'       => $faker->paragraphs(rand(1, 10), true),
+                'excerpt'       => $faker->paragraphs(1, true),
             ]);
         }
     }
